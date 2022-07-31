8f85117c3d2f251717c39baafb12bd63c514e7e9
ashrasmun <ashrasmun@gmail.com>
2022-07-31 18:41:51 +0200

    Prefer VimScript over Lua for key mapping
    This is one of the cases where VimScript seems to bit a bit more reliable the Lua

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,4 +1,28 @@
-vim.g.gui_font_default_size = 16
+--local opts = { noremap = true, silent = true }
+
+--== NEOVIDE ==--
+
+vim.g.neovide_fullscreen = true
+vim.g.neovide_cursor_animation_length = 0.0
+vim.g.neovide_cursor_vfx_mode = 'railgun'
+
+-- The version in VimScript is more reliable. Consider changing it when Lua
+-- works properly.
+vim.cmd([[
+function! NeovideToggleFullscreen()
+    if g:neovide_fullscreen == v:true
+        let g:neovide_fullscreen=v:false
+    else
+        let g:neovide_fullscreen=v:true
+    endif
+endfunction
+
+nnoremap <F11> :call NeovideToggleFullscreen()<CR>
+]])
+
+--== FONT SIZE ==--
+
+vim.g.gui_font_default_size = 12
 vim.g.gui_font_size = vim.g.gui_font_default_size
 vim.g.gui_font_face = "Consolas"
 
@@ -21,8 +45,13 @@ ResetGuiFont()
 
 -- Keymaps
 
-local opts = { noremap = true, silent = true }
-
-vim.keymap.set({'n', 'i'}, "<C-=>", function() ResizeGuiFont(1)  end, opts)
-vim.keymap.set({'n', 'i'}, "<C-->", function() ResizeGuiFont(-1) end, opts)
-vim.keymap.set({'n', 'i'}, "<C-0>", function() ResetGuiFont()    end, opts)
+-- The version in VimScript is more reliable. Consider changing it when Lua
+-- works properly.
+vim.cmd([[
+nnoremap <silent> <F9>  :lua ResizeGuiFont(1)<CR>
+nnoremap <silent> <F10> :lua ResizeGuiFont(-1)<CR>
+nnoremap <silent> <F12> :lua ResetGuiFont()<CR>
+]])
+--vim.keymap.set({'n', 'i'}, "<C-=>", function() ResizeGuiFont(1)  end, opts)
+--vim.keymap.set({'n', 'i'}, "<C-->", function() ResizeGuiFont(-1) end, opts)
+--vim.keymap.set({'n', 'i'}, "<C-0>", function() ResetGuiFont()    end, opts)
