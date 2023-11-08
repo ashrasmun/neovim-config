5ac387117ff857345dd7619e49527d67a7169e75
ashrasmun <ashrasmun@gmail.com>
2023-11-08 20:13:31 +0100

    Enable fancy coloring

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -10,17 +10,9 @@ configs.setup({
     sync_install = false, -- Install parsers synchronously (only applied to `ensure_installed`)
     auto_install = true, -- Automatically install missing parsers when entering buffer
 
-    -- TEMP(ashra, 2023-05-17): This doesn't work :/ It spews out some errors
-    -- about internal stuff...
-    -- highlight = {
-    --     enable = true,
-    --     -- Disable slow treesitter highlight for large files
-    --     disable = function(lang, buf)
-    --         local max_filesize = 100 * 1024 -- 100 KB
-    --         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-    --         if ok and stats and stats.size > max_filesize then
-    --             return true
-    --         end
-    --     end,
-    -- },
+    highlight = {
+        enable = true,
+        -- Can be demanding for the editor, but... the colors!
+        additional_vim_regex_highlighting = true,
+    },
 })
